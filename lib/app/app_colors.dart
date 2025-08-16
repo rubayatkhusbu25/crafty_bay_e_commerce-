560ab8cf189905a76aa9c85e909f290894c1a323
rubayatkhusbu25 <rubayet.khusbu1@gmail.com>
2025-08-16 13:04:34 +0600

    theme added[WIP] & screenutils added for responsiveness

--- lib/app/app_colors.dart
+++ lib/app/app_colors.dart
@@ -1,6 +1,6 @@
 import 'dart:ui';
 
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';  // ✅
 
 class AppColors {
   static Color themeColor = const Color(0xFF07ADAE);
--- lib/app/app_theme.dart
+++ lib/app/app_theme.dart
@@ -1,10 +1,36 @@
+import 'package:crafty_bay_ecommerce/app/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AppTheme {
 
   static ThemeData get lightTheme{
     return ThemeData(
-      primarySwatch:
+     colorScheme: ColorScheme.fromSwatch(
+       primarySwatch: AppColors.getMaterialColor(AppColors.themeColor)
+     ),
+      inputDecorationTheme: InputDecorationTheme(
+        border: _getInputBorder(AppColors.themeColor),
+        focusedBorder: _getInputBorder(AppColors.themeColor),
+        enabledBorder: _getInputBorder(AppColors.themeColor),
+        errorBorder: _getInputBorder(Colors.red),
+        contentPadding: EdgeInsets.symmetric(horizontal: 16.w,vertical: 8.h),
+        hintStyle: TextStyle(
+          fontWeight: FontWeight.w400,
+          color: Colors.grey
+        )
+
+
+      )
+    );
+  }
+
+  static OutlineInputBorder _getInputBorder(Color color ){
+    return OutlineInputBorder(
+      borderSide: BorderSide(
+        color: color,
+        width: 1.2.w
+      )
     );
   }
 }
